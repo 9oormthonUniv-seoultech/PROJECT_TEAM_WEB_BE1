a0b74925a0a9eae47fb659007ea519b49b402920
sooieese <sujinjang609@gmail.com>
2024-08-31 14:03:13 +0900

    인프라구성

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT;
 require('dotenv').config();
 
 const db = mysql.createPool({
-  host: process.env.DB_HOS,
+  host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
